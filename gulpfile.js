98fd916b1f28cf1f060fa18beeb9fe35fc3cbc55
Chris Ashton <chris@blueprintwebtech.com>
2018-05-13 10:28:17 +0100

    Gulp set up. Able to run tasks, but no HTML, styles or scripts to process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,3 @@
-# Read Me
-
-# How this project was set up
-
-## Summary
-1. Install Node, npm, and gulp
-2. Configure `gulpfile.js`
-3. Configure `package.json`
-4. Restore packages
-
-## Details
-
-**1. Install Node, npm, and gulp**
-
- Run following commands, as per [Gulp website](https://gulpjs.com):
-```
-$ ~~npm install gulp-cli -g~~ (already installed on my machine?)
-$ npm install gulp -D
-$ touch gulpfile.js
-$ gulp --help
-```
-
-**2. Configure `gulpfile.js`**
-
-Add following code to `gulpfile.js`:
-
-```javascript
 var gulp = require('gulp');
 var del = require('del');
 var concat = require('gulp-concat');
@@ -109,36 +82,3 @@ gulp.task('build', ['styles', 'scripts', 'images', 'templates']);
 gulp.task('default', ['clean'], function () {
     gulp.start('build');
 });
-```
-
-**3. Configure `packages.json`**
-
-Create `package.json` and add the following content:
-
-```javascript
-{
-  "name": "agidea-frontend-boilerplate-kit",
-  "version": "0.0.1",
-  "description": "Project for Agidea frontend design build.",
-  "dependencies": {},
-  "devDependencies": {
-    "del": "^2.2.2",
-    "gulp": "^3.9.1",
-    "gulp-concat": "^2.6.1",
-    "gulp-cssnano": "^2.1.2",
-    "gulp-imagemin": "^3.2.0",
-    "gulp-sass": "^3.1.0",
-    "gulp-twig": "^0.7.0",
-    "gulp-uglify": "^2.1.2",
-    "gulp-watch": "^4.3.11",
-    "bootstrap-sass": "^3.3.7",
-    "jquery": "^3.2.1"
-  },
-  "scripts": {},
-  "author": "Chris Ashton (Agidea)"
-}
-```
-
-**4. Restore packages**
-
-Run the command `npm install` to estore the packages listed in `package.json`.
